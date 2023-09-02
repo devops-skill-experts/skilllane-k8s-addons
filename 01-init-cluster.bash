37cd88c7aaaa57a00fbf5703b976d6904eb78607
pjames <pjames@gmail.com>
2023-09-02 08:21:43 +0700

    Added logic to check KUBECONFIG var

--- 01-init-cluster.bash
+++ 01-init-cluster.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ ${KUBECONFIG} == "" ]]
+then
+    echo "Please export KUBECONFIG env variable before running script!!!"
+    exit 1
+fi
+
 export $(xargs <.env)
 
 cd "01-init"
